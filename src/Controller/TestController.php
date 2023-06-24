7c0a59137473820a896010ea47ccc8d1a4118431
MercerMorning <hugopochta@gmail.com>
2023-06-24 14:05:47 +0300

    [ioc_testing] tested symfony ioc

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -5,23 +5,29 @@ namespace App\Controller;
 use App\Entity\Order;
 use App\Entity\User;
 use App\Entity\UserProperty;
+use App\Service\FormatService;
+use App\Service\GreeterService;
+use App\Service\MessageService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 class TestController extends AbstractController
 {
-    public function __construct(private readonly EntityManagerInterface $entityManager)
+    public function __construct(
+        private readonly FormatService $formatService,
+        private readonly MessageService $messageService,
+    )
     {
     }
 
     #[Route('/test', name: 'app_test')]
-    public function index(): JsonResponse
+    public function index(): Response
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/TestController.php',
-        ]);
+        $result = $this->formatService->format($this->messageService->printMessages('world'));
+
+        return new Response("<html><body>$result</body></html>");
     }
 }
